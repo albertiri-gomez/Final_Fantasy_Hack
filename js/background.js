121d0eb4cd1dade8f1f9693ea56dc828363a588f
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2019-12-19 17:24:47 +0100

    modulo background

---
+++ js/background.js
@@ -0,0 +1,16 @@
+
+// funcion que define el backgound del juego
+
+function Background(game) {
+    this.game = game
+    this.img = new Image()
+    this.img.src = "img/cloud_background.png"
+    this.x = 0
+    this.y = 0
+
+
+}
+
+Background.prototype.draw = function() {
+    this.game.ctx.drawImage(this.img, this.x, this.y, this.game.canvas.width, this.game.canvas.height)
+}
