80db5e524a37291a2b2464a89f872b2372465a7e
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2019-12-21 10:51:17 +0100

    imageReno

--- js/enemy.js
+++ js/enemy.js
@@ -3,6 +3,7 @@ function Enemy(game) {
     this.x = this.game.canvas.width;
     this.y = this.game.canvas.height - 200;
     this.img = new Image()
+    // this.img.src = "img/Reno.png"
     this.img.src = "img/Reno.png"
     this.dx = 5
     this.w = 170
